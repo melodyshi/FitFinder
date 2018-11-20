4daf3e4d5f51e3faef850763cf1487312b9fa343
Liang Shi <30473359+melodyshi@users.noreply.github.com>
2018-11-20 06:41:46 -0500

    added inline comments

--- parks.py
+++ parks.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 def parks(url):
     """
-        Scrapes "nycgovparks.org" to get information of free sports happening in different parks
+        Scrapes "nycgovparks.org" to get information of free sports happening in different centers
         and saves the result to a dataframe.
         
         Parameters
@@ -33,11 +33,15 @@ def parks(url):
     for center in centers:
         category = center.xpath('.//h4')[0].text
         center_name = center.xpath('.//strong')[0].text
-        address = center.xpath('.//p/text()[1]')[0]
-        weekdays = center.xpath('.//strong[@class="day"]')
+        address = center.xpath('.//p/text()[1]')[0]  #unlabelled child tag
+        
+        #can have multiple weekdays and timeslots
+        weekdays = center.xpath('.//strong[@class="day"]') 
         timeslots = center.xpath('.//span[@class="time"]')
-    
-        for day,timeslot in zip(weekdays,timeslots):
+        
+        #save different schedules for a center as multiple records
+        #loop through weekdays and timeslots together, length should match
+        for day,timeslot in zip(weekdays,timeslots): 
             day = day.text.strip(":")
             time = timeslot.text.strip().split("-")
             start_time = time[0]
