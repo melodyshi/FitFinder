64454373baad7d38829dac6617e0071a3f2cb54f
Liang Shi <30473359+melodyshi@users.noreply.github.com>
2018-12-14 16:31:25 -0500

    Add files via upload

---
+++ FitFinder_Front_End/generate_class_table.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS class_table;
+CREATE TABLE IF NOT EXISTS class_table (
+	center TEXT,
+	address TEXT,
+	class TEXT,
+	day TEXT,
+	start_time TEXT,
+	length TEXT,
+	zip_code TEXT,
+	price TEXT,
+	url TEXT
+);
